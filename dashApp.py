2d2e58d3db4266e1ec836b74866793870aaa8272
Yonatan Diga <digayona@post.bgu.ac.il>
2023-11-14 10:39:20 +0200

    added data until 13.11

--- dashApp.py
+++ dashApp.py
@@ -3,7 +3,7 @@ import plotly.express as px
 import dash
 from dash import dcc, html
 
-CSV_PATH = './csv/csvUntil81123.csv'
+CSV_PATH = './csv/csvUntil131123.csv'
 
 
 def load_and_preprocess_data():
