f054dd70e8ca29b873abed07f417ecd871b72c16
Yonatan Diga <digayona@post.bgu.ac.il>
2024-09-08 16:12:59 +0300

    new way to extract data, dat until 15.10

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+from selenium.webdriver.support import expected_conditions as EC
+
+from selenium.common import TimeoutException
+from selenium.webdriver.support.wait import WebDriverWait
+
 import csv
 import sqlite3
-from time import sleep
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
+##############
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+########
+
 
 from dashApp import startApp
 
@@ -74,6 +88,7 @@ def writeToDB(rowsToSave, file, writer):
     print(f'Done!, added until date and time {newestDate}')
 
 
+
 def getPageContent():
     URL = "https://www.oref.org.il/12481-he/Pakar.aspx"
 
@@ -83,7 +98,7 @@ def getPageContent():
     # driver.find("li", {"id": "lastweek"}).click()
     driver.find_element(By.ID, 'lastweek')
     # driver.find_element_by_id('lastweek').click()
-    sleep(20)
+    #sleep(20)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.close()
@@ -129,6 +144,85 @@ def getPageContent():
     file.close()
 
 
+def firstSettingsToDB2(name):
+    path = 'C:\\Fun projects\\RedAlert\\RedAlertGraphs\\csv\\newData\\' + name
+    file = open(path, 'a', encoding='UTF8')
+    writer = csv.writer(file)
+    # header = ['date', 'city', 'hour', 'minutes']
+    # writer.writerow(header)
+    writer.writerow([])
+    return file, writer
+
+
+def getPageContentNewWay():
+    # Define the API URL
+
+    #Added data until 15.10 including!
+
+    startingDate = "10.10.2023"
+    endingDate = "15.10.2023"
+    api_url = f"https://alerts-history.oref.org.il//Shared/Ajax/GetAlarmsHistory.aspx?lang=he&fromDate={startingDate}&toDate={endingDate}&mode=0 "
+
+    file_rocket, writer_rocket = firstSettingsToDB2('rockets_missiles.csv')
+    file_aircraft, writer_aircraft = firstSettingsToDB2('aircraft_intrusions.csv')
+    file_terrorists, writer_terrorists = firstSettingsToDB2('terrorists_intrusions.csv')
+
+    try:
+        # Fetch the data from the API
+        response = requests.get(api_url)
+        response.raise_for_status()  # Check for request errors
+
+        # Parse the JSON response
+        data = response.json()
+
+        inputStr = input(f'The number of alerts is: {len(data)}, do you wish to stop? ')
+        if len(data) > 1980 or inputStr.lower() == 'p':
+            print("Missing data, choose different dates")
+            return
+
+        # Lists to hold alerts
+        rockets = []
+        aircraft = []
+        terrorists = []
+
+        # Iterate over the items in the JSON response
+        for item in data:
+            date = item.get('date', '')
+            time = item.get('time', '')
+            splittedTime = time.split(':')
+            hour = splittedTime[0]
+            minutes = splittedTime[1]
+            cityName = item.get('data', '')
+            category = item.get('category', '')
+
+            row = [date, cityName, hour, minutes]
+            # Divide data based on category number
+            # Assuming rockets and missiles are category 1, and aircraft penetration is category 2
+            if category == 1:
+                rockets.append(row)
+            elif category == 2:
+                aircraft.append(row)
+            elif category == 10:
+                terrorists.append(row)
+            else:
+                print(f"Unknown category, {item}")
+
+        # Final write for any remaining data
+        if rockets:
+            writeToDB(rockets, file_rocket, writer_rocket)
+
+        if aircraft:
+            writeToDB(aircraft, file_aircraft, writer_aircraft)
+
+        if terrorists:
+            writeToDB(terrorists, file_terrorists, writer_terrorists)
+
+        print("Alerts have been successfully saved into CSV files.")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to fetch alerts: {e}")
+
+
 DATE_COLUMN_NAME = "date"
 CITY_COLUMN_NAME = "city"
 HOUR_COLUMN_NAME = "hour"
@@ -143,7 +237,7 @@ def printNumOfRows(cursor):
     print("Total rows are:  ", len(records))
 
 
-#My Version for Histogram
+# My Version for Histogram
 def showMinutesColumns(cursor):
     # sqlite_select_query = f"""SELECT COUNT({MINUTES_COLUMN_NAME}), {MINUTES_COLUMN_NAME}
     #                           FROM {TABLE_NAME}
@@ -167,19 +261,18 @@ def showMinutesColumns(cursor):
 
     plt.xticks(np.arange(0, 60, 5))
     plt.yticks(np.arange(0, max(y), 50))
-    plt.bar(x, y, color ='blue', width = 0.8)
+    plt.bar(x, y, color='blue', width=0.8)
 
     # Saving a plotted graph as an Image
     plt.savefig('minutesGraph (With 7-10).png')
     plt.show()
 
 
-
-#Utilizes the Histogram in matplotlib
+# Utilizes the Histogram in matplotlib
 def hoursHist(cursor):
     sqlite_select_query = f"""SELECT {HOUR_COLUMN_NAME} from {TABLE_NAME}
                               WHERE not {DATE_COLUMN_NAME} = '07.10.2023' """
-    #sqlite_select_query = f"""SELECT {HOUR_COLUMN_NAME} from {TABLE_NAME} """
+    # sqlite_select_query = f"""SELECT {HOUR_COLUMN_NAME} from {TABLE_NAME} """
 
     cursor.execute(sqlite_select_query)
     records = cursor.fetchall()
@@ -191,23 +284,24 @@ def hoursHist(cursor):
     plt.title("Number of Alerts in a specific Hour")
     plt.xlabel('Hour')
     plt.ylabel("Num of alerts")
-    plt.hist(x, 24, color='blue', width= 0.8)
+    plt.hist(x, 24, color='blue', width=0.8)
     plt.xticks(np.arange(0, 24))
     plt.savefig('hourGraph.png')
     plt.show()
 
 
 def plotGraphs():
+    global sqliteConnection
     try:
-        #sqliteConnection = sqlite3.connect('alerts.db')
+        # sqliteConnection = sqlite3.connect('alerts.db')
         sqliteConnection = sqlite3.connect('csv\\alerts.db')
         cursor = sqliteConnection.cursor()
         print("Connected to SQLite")
 
-        #printNumOfRows(cursor)
+        # printNumOfRows(cursor)
 
         showMinutesColumns(cursor)
-        #hoursHist(cursor)
+        # hoursHist(cursor)
 
         cursor.close()
 
@@ -220,6 +314,7 @@ def plotGraphs():
 
 
 if __name__ == "__main__":
-    #getPageContent()
-    #plotGraphs()
-    startApp()
+    getPageContentNewWay()
+    # getPageContent()
+    # plotGraphs()
+    # startApp()
